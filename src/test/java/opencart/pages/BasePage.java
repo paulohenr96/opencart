453a34b7da789a15d3105b6f18a1d1688756b0d6
Paulo Henrique <paulohrq_96@hotmail.com>
2025-01-31 19:37:22 -0300

    add: homepage

--- src/test/java/opencart/pages/BasePage.java
+++ src/test/java/opencart/pages/BasePage.java
@@ -1,5 +1,6 @@
 package opencart.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class BasePage {
@@ -9,4 +10,15 @@ public class BasePage {
 	public BasePage (WebDriver driver) {
 		this.driver=driver;
 	}
+	
+	
+	public void click(By locator) {
+		driver.findElement(locator).click();
+	}
+	
+	public void set(By locator,String text) {
+		driver.findElement(locator).sendKeys(text);
+	}
+	
+	
 }
--- src/test/java/opencart/pages/HomePage.java
+++ src/test/java/opencart/pages/HomePage.java
@@ -1,17 +1,39 @@
 package opencart.pages;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
-public class HomePage {
+public class HomePage extends BasePage {
 
-	@FindBy(xpath = "//div[@class='col text-end']//i[@class='fa-solid fa-caret-down']")
-	private WebElement myAccount;
-	@FindBy(xpath = "//a[normalize-space()='Register']")
-	private WebElement register;
-	@FindBy(xpath = "//a[normalize-space()='Login']")
-	private WebElement login;
+	private By btnMyAccount = By.xpath("//i[@class='fa-solid fa-user']");
+	private By btnRegister = By.xpath("//a[normalize-space()='Register']");
+	private By btnLogin = By.xpath("//a[normalize-space()='Login']");
+	private By fieldSearch=By.xpath("//input[@placeholder='Search']");
+	private By btnSearch=By.xpath("//button[@class='btn btn-light btn-lg']");
+	
+	public HomePage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
 	
 	
+	public void clickMyAccount() {
+		click(btnMyAccount);
+	}
+	public void clickRegister() {
+		click(btnRegister);
+	}
 	
+	public void clickLogin() {
+		click(btnLogin);
+	}
+	
+	public void writeSearchBar(String item) {
+		set(fieldSearch,item);
+	}
+	
+	public void clickSearch() {
+		click(btnSearch);
+	}
+
 }
